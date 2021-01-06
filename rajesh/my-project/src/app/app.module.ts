16c087968f7dbabe51e75b349b731abd2bdc65ba
rajeshrajpoot123 <rajesh.aussizz@gmail.com>
2021-01-06 17:57:06 +0530

    new

--- rajesh/my-project/src/app/app.module.ts
+++ rajesh/my-project/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RajeshComponent } from './rajesh/rajesh.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RajeshComponent
   ],
   imports: [
     BrowserModule,
