7237c547b404e3f3f9f27b766cfa360555ba42e7
rajeshrajpoot123 <rajesh.aussizz@gmail.com>
2021-01-06 17:37:43 +0530

    new

--- rajesh/my-project/src/app/app.module.ts
+++ rajesh/my-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RajeshComponent } from './rajesh/rajesh.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RajeshComponent
   ],
   imports: [
     BrowserModule,
---
+++ rajesh/my-project/src/app/rajesh/rajesh.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rajesh',
+  templateUrl: './rajesh.component.html',
+  styleUrls: ['./rajesh.component.scss']
+})
+export class RajeshComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
